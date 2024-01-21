08b333ec349591dcf76700b420fe067e52f76355
Balázs Bence <balazsbence.sas@gmail.com>
2024-01-21 14:50:31 +0100

    feat: implement custom rules

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+  root: true,
+  ignorePatterns: ['**/dist/**', './node_modules', './.vscode'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-plugin-import/recommended',
+    'plugin:rxjs/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
+  rules: {
+    'tsdoc/syntax': 'warn',
+    'import/no-unresolved': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: ['./tsconfig.json'] },
+};
